7af363148818df4bbecdfbf5808eba0e532a8f50
Weronika Żak <weronikarzak@gmail.com>
2019-07-20 22:56:25 +0200

    add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
   constructor(
     private messageService: MessageService,
@@ -18,35 +21,44 @@ export class HeroService {
     getHeroes (): Observable<Hero[]> {
       return this.http.get<Hero[]>(this.heroesUrl)
         .pipe(
+          tap(_ => this.log('fetched heroes')),
           catchError(this.handleError<Hero[]>('getHeroes', []))
         );
     }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add('HeroService: fetched hero id=${id}');
-    return of(HEROES.find(hero => hero.id === id));
+    const url = '${this.heroesUrl}/${id}';
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log('fetched hero id=${id}')),
+      catchError(this.handleError<Hero>('getHero id=${id}'))
+    );
   }
 
   private log(message: string) {
     this.messageService.add('HeroService: ${message}');
   }
 
-  /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(tap(_ => this.log('update hero id=${hero.id}')),
+      catchError(this.handleError<any>('updateError'))
+      );
+  }
 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+        console.error(error);
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
+        this.log('${operation');
+
+        return of(result as T);
     };
+  }
+
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log('added hero w/ id=${newHero.id}')),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 }
